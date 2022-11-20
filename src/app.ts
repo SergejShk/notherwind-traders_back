f16733386d69411542dbb85f97904efa6b070e28
SergejShk <sshkurupij11@gmail.com>
2022-11-20 17:31:03 +0200

    upd vercel, package

--- src/app.ts
+++ src/app.ts
@@ -16,11 +16,11 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/app/suppliers", supplierRouter);
-app.use("/app/products", productsRouter);
-app.use("/app/orders", ordersRouter);
-app.use("/app/employees", employeesRouter);
-app.use("/app/customers", customersRouter);
+app.use("/api/suppliers", supplierRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/orders", ordersRouter);
+app.use("/api/employees", employeesRouter);
+app.use("/api/customers", customersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not Found" });
