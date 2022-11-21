36d2fd0e0fb09caa3b5bba17f4dc4ba601f8d05c
SergejShk <sshkurupij11@gmail.com>
2022-11-22 00:17:29 +0200

    turned off database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import logger from "morgan";
 import cors from "cors";
-import supplierRouter from "./routes/suppliersRoutes";
-import productsRouter from "./routes/productsRoutes";
-import ordersRouter from "./routes/ordersRoutes";
-import employeesRouter from "./routes/employeesRoutes";
-import customersRouter from "./routes/customersRoutes";
+// import supplierRouter from "./routes/suppliersRoutes";
+// import productsRouter from "./routes/productsRoutes";
+// import ordersRouter from "./routes/ordersRoutes";
+// import employeesRouter from "./routes/employeesRoutes";
+// import customersRouter from "./routes/customersRoutes";
 import { errorHandler } from "./helpers/errorHandlers";
 
 export const app = express();
@@ -16,11 +16,15 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/suppliers", supplierRouter);
-app.use("/api/products", productsRouter);
-app.use("/api/orders", ordersRouter);
-app.use("/api/employees", employeesRouter);
-app.use("/api/customers", customersRouter);
+app.get("/", (_req, res, _next) => {
+  res.status(200).json("Seccessfully");
+});
+
+// app.use("/api/suppliers", supplierRouter);
+// app.use("/api/products", productsRouter);
+// app.use("/api/orders", ordersRouter);
+// app.use("/api/employees", employeesRouter);
+// app.use("/api/customers", customersRouter);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Not Found" });
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
 import "reflect-metadata";
 import dotenv from "dotenv";
 import { app } from "./app";
-import { AppDataSource } from "./db/dbSourse";
+// import { AppDataSource } from "./db/dbSourse";
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 const serverStart = async () => {
   try {
-    await AppDataSource.initialize();
+    // await AppDataSource.initialize();
 
     app.listen(PORT, () => {
       console.log(`Server listening on port ${PORT}`);
